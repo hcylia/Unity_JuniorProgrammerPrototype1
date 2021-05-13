dce7d959c9f042e8874bc0b70102912a2ad8c45b
Cylia <hcylia@outlook.fr>
2021-05-13 10:10:21 +0200

    Camera angle changing v2

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,34 +5,19 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offsetTopView = new Vector3(0, 5, -7);
-    private Vector3 offsetPlayerView = new Vector3(0, 2.4f, 0.4f);
-    private Vector3 usedOffset;
-    private float lastFrame;
+    private Vector3 offset = new Vector3(0, 5, -7);
+    
     // Start is called before the first frame update
     void Start()
     {
-        usedOffset = offsetTopView;
-        lastFrame = Time.frameCount;
+        
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
         // Offset the camera behind the player by adding to the player's position
-        transform.position = player.transform.position + usedOffset;
-        if (Time.frameCount - lastFrame > 5)
-        {
-            lastFrame = Time.frameCount;
-            if (Input.GetKey("c") && usedOffset != offsetPlayerView)
-            {
-                usedOffset = offsetPlayerView;
-            }
-            else if (Input.GetKey("c") && usedOffset != offsetTopView)
-            {
-                usedOffset = offsetTopView;
-            }
-        }
+        transform.position = player.transform.position + offset;
         
     }
 }
---
+++ Assets/Scripts/ViewCameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewCameraController : MonoBehaviour
+{
+    public Camera topView;
+    public Camera playerView;
+    private float lastFrame;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastFrame = Time.frameCount;
+        topView.enabled = true;
+        playerView.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.frameCount - lastFrame > 5)
+        {
+            lastFrame = Time.frameCount;
+            if (Input.GetKey("c"))
+            {
+                topView.enabled = !topView.enabled;
+                playerView.enabled = !playerView.enabled;
+            }
+        }
+    }
+}
